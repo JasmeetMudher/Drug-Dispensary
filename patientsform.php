df8e20bc7c06f196e86659bffa5a16c2adf966c5
JasmeetMudher <jsmudher4@gmail.com>
2023-05-28 16:33:12 +0300

    Added patientsform.php

---
+++ patientsform.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="connection.php" method="">
+        <label>First Name:</label><input type="text" name="First Name"><br>
+        <label>Last Name:</label><input type="text" name="Last Name"><br>
+        <label>SSN:</label><input type="text" name="SSN"><br>
+        <label>DOB:</label><input type="date" name="DOB"><br>
+        <label>Address:</label><input type="text" name="Address"><br>
+        <label>Phone:</label><input type="text" name="Phone"><br>
+        <label>Email:</label><input type="text" name="Email"><br>
+        <label>Gender:</label><input type="text" name="Gender"><br>
+        <button type="submit" name="Submit">Submit</button>
+    </form>
+</body>
+</html>
