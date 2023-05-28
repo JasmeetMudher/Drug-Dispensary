a8fb6c77bee948b2e2cc67cb48f0d2c0f180ebc3
Winstone Were <stoniedev@gmail.com>
2023-05-28 19:35:58 +0300

    added connection.php

---
+++ connection.php
@@ -0,0 +1,19 @@
+
+<?php 
+    $host = "localhost";
+    $username = "root";
+    $pass = "123pass";
+    $dbname = "drugdispensary";
+
+
+    $conn = mysqli_connect($host, $username, $pass, $dbname);
+
+    if($conn == true) {
+        echo " ";
+    }else {
+        die("failed to connect");
+    }
+
+/* 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 */
+
+?>
