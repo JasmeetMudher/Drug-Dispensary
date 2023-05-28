dc322a7fe157ec960d501c833cc3751a64c4dd27
JasmeetMudher <jsmudher4@gmail.com>
2023-05-28 16:57:21 +0300

    Added patientsform.php

--- patientsform.php
+++ patientsform.php
@@ -1,3 +1,24 @@
+<?php
+require 'connection.php';
+if(isset($_POST["submit"])){
+    $FirstName = $_POST["FirstName"];
+    $LastName = $_POST["LastName"];
+    $SSN = $_POST["SSN"];
+    $DOB = date('d-m-Y',strtotime($_POST["DOB"]));
+    $Address = $_POST["Address"];
+    $Phone = $_POST["Phone"];
+    $Email = $_POST["Email"];
+    $Gender = $_POST["Gender"];
+
+    $query = "INSERT INTO patients VALUES('$FirstName','$LastName',$SSN,'$DOB','$Address','$Phone','$Email','$Gender')";
+    if(mysqli_query($conn,$sql)){
+        echo "New Record added";
+    }else{
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,16 +28,16 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="connection.php" method="">
-        <label>First Name:</label><input type="text" name="First Name"><br>
-        <label>Last Name:</label><input type="text" name="Last Name"><br>
+    <form action="connection.php" method="post">
+        <label>First Name:</label><input type="text" name="FirstName"><br>
+        <label>Last Name:</label><input type="text" name="LastName"><br>
         <label>SSN:</label><input type="text" name="SSN"><br>
         <label>DOB:</label><input type="date" name="DOB"><br>
         <label>Address:</label><input type="text" name="Address"><br>
         <label>Phone:</label><input type="text" name="Phone"><br>
         <label>Email:</label><input type="text" name="Email"><br>
         <label>Gender:</label><input type="text" name="Gender"><br>
-        <button type="submit" name="Submit">Submit</button>
+        <button type="submit" name="submit">Submit</button>
     </form>
 </body>
 </html>
